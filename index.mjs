41da7afe8119edd7202ebb0a236a4624fd47118a
hamza-25 <me677499@gmail.com>
2025-05-15 20:06:50 +0100

    first command setup

--- index.mjs
+++ index.mjs
@@ -5,21 +5,18 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('needjs')
+  .name('needJs')
   .description('A CLI application built with Commander.js')
   .version('1.0.0')
-  .option('-d, --debug', 'output extra debugging information')
-  .option('-f, --file <path>', 'specify the file to process')
-  .option('-t, --timeout <seconds>', 'specify the timeout in seconds', '60');
+  .option('-v, --verbose', 'enable verbose output');
+
+program
+  .command('struct <structureType>')
+  .description('create a structure type (MVC, MVVM,etc.)')
+  .action((structureType) => {
+    console.log(structureType);
+  });
+  
+program.parse();
 // Access the options
 const options = program.opts();
-
-// build command line arguments
-program
-  .command('structure')
-  .description('Generate a project structure, including a README file')
-  .option('-t, --type <type>', 'specify the type of project (e.g., mvc, api)')
-  .action(() => {
-    console.log('Generating project structure...');
-    // console.log(`Project type: ${options.type}`);
-  });
