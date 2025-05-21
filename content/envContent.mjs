fa681c09e388dd203dc9d71ada852381caf5540c
hamza-25 <me677499@gmail.com>
2025-05-21 18:53:45 +0100

    add envContent file

---
+++ content/envContent.mjs
@@ -0,0 +1,10 @@
+export const envContent = () => {
+    return `
+PORT=3000
+NODE_ENV=development
+DB_HOST=localhost
+DB_PORT=3306
+DB_USER=yourusername
+DB_PASSWORD=yourpassword
+DB_NAME=yourdbname`;
+}
