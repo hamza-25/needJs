9acaaecb8d5c7e4ce6427baa825fead18c738f2d
hamza-25 <me677499@gmail.com>
2025-05-23 19:28:50 +0100

    add consoleStyle.mjs

--- commands/setSecurity.mjs
+++ commands/setSecurity.mjs
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import consoleStyle from '../utils/consoleStyle.mjs';
 
 const setSecurity =  new Command('security')
   .description('set security helmet, cors, cors-parser, hpp, xss-clean, etc.')
@@ -8,7 +9,8 @@ const setSecurity =  new Command('security')
     const entryFile = entryPoint || 'index.js';
     console.log(entryFile);
     console.log(`Setting security...`);
-    console.log(`Note: make sure you ran this command. 'npm i helmet cors cors-parser hpp xss-clean'`);
+    const msg = consoleStyle('\'npm i helmet cors cors-parser hpp xss-clean\'', 'green', true);
+    console.log(`Note: make sure you ran this command. ${msg}`);
   });
 
 export default setSecurity;
