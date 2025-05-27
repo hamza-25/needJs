f378a392285540ca4c760d77d80e559c2bd44cb5
hamza-25 <me677499@gmail.com>
2025-05-27 21:24:46 +0100

    add content to mongo content

--- content/mongoContent.mjs
+++ content/mongoContent.mjs
@@ -1,3 +1,24 @@
 export const mongoContent = () => {
-    return ``;
+    return `
+const mongoose = require('mongoose');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || '27017';
+const dbName = process.env.DB_NAME || 'mydb';
+
+const connectMongo = async () => {
+  try {
+    await mongoose.connect(\`mongodb://${host}:${port}/${dbName}\`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+};
+
+module.exports = connectMongo;
+    
+`;
 }
