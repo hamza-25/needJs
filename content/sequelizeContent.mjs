6da13d8565e086c45f623abce09b4e093b8108ed
hamza-25 <me677499@gmail.com>
2025-05-27 21:12:44 +0100

    add content to sequelize file

--- content/sequelizeContent.mjs
+++ content/sequelizeContent.mjs
@@ -1,3 +1,15 @@
-export const sequelizeContent = () => {
-    return ``;
+export const sequelizeContent = (dialect) => {
+    return `
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: dialect || 'mysql',
+  port: process.env.DB_PORT || 3306,
+  logging: false,
+});
+
+export default sequelize;
+
+`;
 }
